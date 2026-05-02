3f35b5d11857b0d1c7cf9a67dd041f3f53c0b747
ericraynaldo <raynaldoeric@gmail.com>
2026-05-03 00:15:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener("fetch", function(event) {
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request));
 });
